8f7f281a1e980e7b4c8fb9f977f52c6777ba8c1f
TyAiTi <tuhoangty1@gmail.com>
2021-11-03 01:22:01 +0700

    test form hook validation

--- blog/src/components/form/Form.js
+++ blog/src/components/form/Form.js
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
-    
+import s from "./Form.module.css"
+
   
 export default function Form(){
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
@@ -10,12 +11,24 @@ export default function Form(){
     // console.log(watch("firstname")); //xem nguoi dung input
     return(
         <div>
-            <h4>Đây là react form validation </h4>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            {/* <div>
+                
+            </div> */}
+            {/* <h4>Đây là react form validation </h4> */}
+            {/* <form onSubmit={handleSubmit(onSubmit)}>
                 <input  {...register("name", { required: true })} />
                 <input {...register("firstname", { required: true })} />
                 <input type="submit" />
-            </form>
+            </form> */}
+            
+
+            <div>
+                <form className={s.top} onSubmit={handleSubmit(onSubmit)}>
+                    <input className={s.shareInput} {...register("content", { required: true })} />
+                    <input className={s.shareMoney} {...register("benefit", { required: true })} />
+                    <input className={s.button_default} type="submit" />
+                </form>
+            </div>
             {errors.name && <span>This field is name</span>}
             {errors.firstname && <span>This field is firstname</span>}
         </div>
