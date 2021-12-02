3b02130923b35675222a29ffb0fffe0f35c4f206
TyAiTi <tuhoangty1@gmail.com>
2021-12-02 22:23:55 +0700

    xong 4 hinh

--- blog/src/views/TabInfor/GirdBasic.js
+++ blog/src/views/TabInfor/GirdBasic.js
@@ -5,6 +5,8 @@ import Typography from '@material-ui/core/Typography';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
 import CheckCircleOutlineIcon  from '@mui/icons-material/CheckCircleOutline';
 
+const src_img = "http://localhost/code/baohiem/check/iphone.jpg"
+
 const useStyles2 = makeStyles( (theme)=>({
     root: {
       flexGrow: 1,
@@ -77,6 +79,38 @@ const useStyles2 = makeStyles( (theme)=>({
         fontSize:'16px',
         color: '#323C4A',
     },
+    imgOCR:{
+        height: '61px',
+        width: '52px',
+    },
+    imgAI:{
+        height: '61px',
+        width: '61px',
+    },
+    img1:{
+        margin: '10px 0px 0px 8px'
+    },
+    img2:{
+        margin: '10px 0px 0px 13px'
+    },
+    img3:{
+        margin: '13px 0px 0px 8px'
+    },
+    img4:{
+        margin: '13px 0px 0px 13px'
+    },
+    img5:{
+        margin: '10px 0px 0px 0px'
+    },
+    img6:{
+        margin: '10px 0px 0px 13px'
+    },
+    img7:{
+        margin: '13px 0px 0px 0px'
+    },
+    img8:{
+        margin: '13px 0px 0px 13px'
+    }
   }))
 export default function GridBasic(){
     const classes2 = useStyles2()
@@ -599,6 +633,180 @@ export default function GridBasic(){
                     </Grid>
                 </Grid>
                 
+                {/* Bảng dưới cùng */}
+                <Grid container item sx={12} md={12} className={classes2.tabledown}>
+                    {/* Bảng trái */}
+                    <Grid container item sx={12} md={7} >
+                        <Grid item sx={12} md={12} className={classes2.framecontent}>
+                            <Typography className={classes2.textHeader}>
+                                Thông tin xác thực
+                            </Typography>
+                        </Grid>
+                        <Grid container item sx={12} md={4} className={classes2.framecontent}>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textReq}>
+                                    Xác thực IMEI
+                                </Typography>
+                                </Grid>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textRes}>
+                                    Có
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid container item sx={12} md={4} className={classes2.framecontent}>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textReqNon}>
+                                    Giám định vỡ màn hình
+                                </Typography>
+                                </Grid>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textResNon}>
+                                    Không
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid container item sx={12} md={4} className={classes2.framecontent}>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textReqNon}>
+                                    Thời gian khách hàng yêu cầu
+                                </Typography>
+                                </Grid>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textResNon}>
+                                    11/10/2021 00:00
+                                </Typography>
+                            </Grid>
+                        </Grid>
+
+                        <Grid container item sx={12} md={4} className={classes2.framecontent}>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textReq}>
+                                    Kết quả OCR
+                                </Typography>
+                                </Grid>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textRes}>
+                                    Thành công
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid container item sx={12} md={4} className={classes2.framecontent}>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textReqNon}>
+                                    Kết quả AI
+                                </Typography>
+                                </Grid>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textResNon}>
+                                    Không vỡ
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid container item sx={12} md={4} className={classes2.framecontent}>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textReqNon}>
+                                    Thời gian hệ thống trả kết quả 
+                                </Typography>
+                                </Grid>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textResNon}>
+                                    11/10/2021 02:00
+                                </Typography>
+                            </Grid>
+                        </Grid>
+
+                        <Grid container item sx={12} md={4} className={classes2.framecontent}>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textReq}>
+                                    OCR - IMEI1
+                                </Typography>
+                                </Grid>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textRes}>
+                                    123123123123
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid container item sx={12} md={4} className={classes2.framecontent}>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textReqNon}>
+                                    Tỉ lệ chính xác
+                                </Typography>
+                                </Grid>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textResNon}>
+                                    90,00 %
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        
+                        <Grid item sx={12} md={12} style={{marginBottom:'20px'}}>
+                            {/* Khoảng cách giữa trên ảnh */}
+                        </Grid>
+
+                        <Grid container item sx={12} md={4} className={classes2.framecontent}>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textReq}>
+                                    Ảnh OCR
+                                </Typography>
+                            </Grid>
+                            <Grid container item sx={12} md={12}>
+                                <Grid item sx={12}  >
+                                    <img src={""} 
+                                    className={`${classes2.imgOCR} ${classes2.img1}`}/>
+                                </Grid>
+                                <Grid item sx={12}  >
+                                    <img src={""} 
+                                    className={`${classes2.imgOCR} ${classes2.img2}`}/>
+                                </Grid>
+                                <Grid item sx={12} md={4}>  </Grid>
+                                <Grid item sx={12}  >
+                                    <img src={""} 
+                                    className={`${classes2.imgOCR} ${classes2.img3}`}/>
+                                </Grid>
+                                <Grid item sx={12}  >
+                                    <img src={""} 
+                                    className={`${classes2.imgOCR} ${classes2.img4}`}/>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid container item sx={12} md={4} className={classes2.framecontent}>
+                            <Grid item sx={12} md={12}>
+                                <Typography className={classes2.textReqNon}>
+                                    Ảnh AI
+                                </Typography>
+                            </Grid>
+                            <Grid container item sx={12} md={12}>
+                                <Grid item sx={12} >
+                                    <img src={""} 
+                                    className={`${classes2.imgAI} ${classes2.img5}`}/>
+                                </Grid>
+                                <Grid item sx={12}  >
+                                    <img src={""} 
+                                    className={`${classes2.imgAI} ${classes2.img6}`}/>
+                                </Grid>
+                                <Grid item sx={12} md={3}>  </Grid>
+                                <Grid item sx={12}  >
+                                    <img src={""} 
+                                    className={`${classes2.imgAI} ${classes2.img7}`}/>
+                                </Grid>
+                                <Grid item sx={12}  >
+                                    <img src={""} 
+                                    className={`${classes2.imgAI} ${classes2.img8}`}/>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item sx={12} md={12} style={{marginBottom:'20px'}}>
+                            {/* Khoảng cách cho footer */}
+                        </Grid>
+                    </Grid>
+                    {/* Bảng phải để trống */}
+                    <Grid container item sx={12} md={5} >
+
+                    </Grid>
+                </Grid>
+
             </Grid>
         </div>
     )
